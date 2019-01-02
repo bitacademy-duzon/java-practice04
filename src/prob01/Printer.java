01230a428c0fc49faeb7ea27e3c8d26bcc9a3d5b
GiDeok <gdtbgl93@gmail.com>
2019-01-02 17:46:01 +0900

    4일차 과제 완료

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -1,4 +1,15 @@
 package prob01;
 
+import java.util.Scanner;
+
 public class Printer	 {
+	
+	
+	public void println(Object arg) {
+		
+		System.out.println(arg.toString());
+		
+		
+	}
+	
 }
--- src/prob02/Book.java
+++ src/prob02/Book.java
@@ -1,4 +1,55 @@
 package prob02;
 
 public class Book {
+
+	private int bookNo;
+	private String title;
+	private String author;
+	private int stateCode;
+
+	public Book(int bookNo, String title, String author) {
+		super();
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+		stateCode = 1;
+	}
+
+	public void rent() {
+		System.out.println(this.title + " 가 대여 되었습니다.");
+		stateCode = 0;
+
+	}
+
+	public void print() {
+
+		String token = stateCode == 1 ? "재고있음" : "대여중";
+		System.out.println(token);
+
+	}
+
+	public int getBookNo() {
+		return bookNo;
+	}
+
+	public void setBookNo(int bookNo) {
+		this.bookNo = bookNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
 }
