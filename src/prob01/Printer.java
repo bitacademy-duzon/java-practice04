d82d7df49c09ba8f987dea5c13c88794f2458fd2
BIT <BIT@DESKTOP-4M0FKCM>
2019-01-04 09:27:38 +0900

    generic method & variable parameter

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -1,19 +1,40 @@
 package prob01;
 
 public class Printer{
-	public void println( int val ) {
-		System.out.println( val );
-	}
-
-	public void println( boolean val ) {
-		System.out.println( val );
+//	public void println( int val ) {
+//		System.out.println( val );
+//	}
+//
+//	public void println( boolean val ) {
+//		System.out.println( val );
+//	}
+//	
+//	public void println( String val ) {
+//		System.out.println( val );
+//	}
+//	
+//	public void println( double val ) {
+//		System.out.println( val );
+//	}
+	
+//	public <T> void println( T t ) {
+//		System.out.println( t );
+//	}
+	
+	// 가변 변수
+	public Integer sum(Integer... nums) {
+		Integer sum = 0;
+		for(Integer i : nums) {
+			sum += i;
+		}
+		return sum;
 	}
 	
-	public void println( String val ) {
-		System.out.println( val );
+	@SuppressWarnings("unchecked")
+	public <T> void println( T... ts ) {
+		for(T t : ts) {
+			System.out.println( t );
+		}
 	}
 	
-	public void println( double val ) {
-		System.out.println( val );
-	}	
 }
